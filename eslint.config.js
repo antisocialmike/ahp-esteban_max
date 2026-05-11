e44181a129e51387f779c623334dc90e7503b04d
antisocialmike <migueldelgadillo216@gmail.com>
2026-05-11 12:30:05 -0600

    ok pero necesito q funcione

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,8 @@
-module.exports = [
+export default [
     {
         rules: {
-            "no-unused-vars": "warn",
-            "no-undef": "warn"
+            "no-unused-vars": "error",
+            "no-undef": "error"
         }
     }
 ];
